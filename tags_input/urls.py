a8488838ae3374cfc1a26d7c60e220ece51f4c62
Cangiani Agustín <acangiani@routeatlas.com>
2014-01-14 16:37:36 -0300

    Added __version__ to init (0.0.1)

--- tags_input/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('tags_input.views',
-    url(r'^autocomplete/(?P<app>\w+)/(?P<model>\w+)/(?P<fields>[\w-]+)/$',
-        'autocomplete', name='autocomplete'),
-)
-
