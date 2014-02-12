f5821420cfdd14e1c85d8d33b17c9fc691573545
Angel Velasquez <angvp@archlinux.org>
2014-02-12 12:56:49 -0300

    Remove .pyc from the repo

--- runtests.py
+++ runtests.py
@@ -15,12 +15,9 @@ if not settings.configured:
         INSTALLED_APPS=(
             'django.contrib.contenttypes',
             'django.contrib.auth',
-            'oml',
         ),
         SITE_ID=1,
         SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
-        OML_CONFIG = {'OML_EXCLUDE_MODERATED': True,
-                          'OML_EXCLUDED_GROUPS': [1,]}
     )
 
 
