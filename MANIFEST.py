164cd44ff2b765fd43d4305bd3494e3044ef7e75
Cangiani Agustín <acangiani@routeatlas.com>
2014-03-27 12:08:50 -0300

    Fix - Version bump

--- MANIFEST.py
+++ MANIFEST.py
@@ -1,6 +1,6 @@
-include arrayfieldtags_input
-recursive-include arrayfieldtags_input *.py
-recursive-include arrayfieldtags_input/templates *.html
-recursive-include arrayfieldtags_input/static/js *.js
-recursive-include arrayfieldtags_input/static/css *.css
-recursive-include arrayfieldtags_input/static/css/base/images *.png
+include arrayfield_tagsinput
+recursive-include arrayfield_tagsinput *.py
+recursive-include arrayfield_tagsinput/templates *.html
+recursive-include arrayfield_tagsinput/static/js *.js
+recursive-include arrayfield_tagsinput/static/css *.css
+recursive-include arrayfield_tagsinput/static/css/base/images *.png
